70c528540dad9ed515937515570ed8940ae1bfa6
Mahdi <88978694+darkkLUCIFER@users.noreply.github.com>
2022-09-06 20:38:36 +0430

    converting json data into python dictionary using json librarry

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import requests
+import json
 from config import url, API_KEY
 
 
 def get_rates():
     response = requests.get(url, params={'apikey': API_KEY})
-    json_info = response.text
-    print(json_info)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    return None
 
 
 if __name__ == '__main__':
-    get_rates()
+    print(get_rates())
